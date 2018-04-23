e6b91badf553290fda3a3a5fd7b503110b998d3b
sebas3552 <38670360+sebas3552@users.noreply.github.com>
2018-04-23 15:27:49 -0600

    Update Diccionario.h

--- Diccionario.h
+++ Diccionario.h
@@ -1,6 +1,7 @@
 #ifndef DICCIONARIO
 #define DICCIONARIO
 #include <string>
+#include "Arbol.h"
 class Diccionario
 {
 
@@ -11,7 +12,8 @@ class Diccionario
 		
 	
 	private:
-		const char caracteres[33] = {};
+		const char caracteres[33] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r',
+					    's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'á', 'é', 'í', 'ó', 'ú', 'ü', 'ñ', '$'};
 	
 };
 #endif
