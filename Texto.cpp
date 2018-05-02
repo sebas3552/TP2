3601a711088958038d1f8ee1dcf41444615d9ec9
sebas3552 <38670360+sebas3552@users.noreply.github.com>
2018-05-02 17:36:03 -0600

    Update Texto.cpp

--- Texto.cpp
+++ Texto.cpp
@@ -1,6 +1,5 @@
 #include "Texto.h"
 #include <iostream>
-#include <stdexcept>
 
 using namespace std;
 
@@ -19,18 +18,84 @@ Diccionario &Texto::cargarDiccionario(string nombreDiccionario, Diccionario &dic
 	string s;
 	int palabrasAgregadas = 0;
 	while(getline( archivoDiccionario, s )){
-		try{
-			diccionario += convertirMinusculas(s);
-			palabrasAgregadas++;
-		}catch(bad_alloc &e){
-			cerr << "Error fatal!: sin memoria disponible" << endl;
-			exit(1);
-		}
-		
+		diccionario += convertirMinusculas(s);
+		palabrasAgregadas++;
 	}
 	archivoDiccionario.close();
 	cout << "Se agregaron " << palabrasAgregadas << " palabras al diccionario!" << endl;
 	return diccionario;
+}
+
+void Texto::metodoDivision( string nombreArchivo ){
+	fstream archivo( nombreArchivo );
+	Palabra p;                                //para acceder al vector de caracteres validos
+	
+	if( !archivo ){
+		throw invalid_argument( "El archivo indicado no existe!" );
+	}
+	
+	string s;
+	while( getline( archivo, s ) ){
+		const char * v = s.c_str();
+		bool hayEspacios = false;
+		bool hayRaros = false;
+		
+		for( int i = 0; i < s.length(); i++ ){
+			if( v[i] == ' ' ){
+				hayEspacios = true;
+			}
+			
+			if( esRaro( (v[i] < 0? esRaro(Palabra::determinarCaracter(v[++i])) : v[i]) ){
+				hayRaros = true;
+			}
+		}
+		
+		if( hayEspacios ){
+			//divPorEspacios( s );
+			cout << "tiene espacios, entonces se debe llamar al metodo que divida por espacios" << endl;
+		} else if( hayRaros ){
+			//divPorRaros( s );
+			cout << "tiene raros y no espacios, entonces se llama al metodo que divide por raros" << endl;
+		} else {
+			//divPorDiccionario( s );
+			cout << "no tiene ni espacios ni raros" << endl;
+		}
+	}
+}
+
+void Texto::divPorEspacios( string aDividir ){
+	vector <string> divisiones;
+	
+	divisiones.push_back( aDividir );
+	
+	int cantDiv = 0;
+	while( strtok( aDividir.c_str(), " " ) ){
+		//divisiones.push_back( div );
+		cantDiv++;
+	}
+	
+	for( int i = 0; i < cantDiv; i++ ){
+		string z = strtok( aDividir.c_str(), " " );
+		divisiones.push_back( z );
+	}
+	
+	for( int i = 0; i < cantDiv; i++ ){
+		cout << divisiones[i] << endl;
+	}
+	
+	//return divisiones;
+}
+
+bool Texto::esRaro( char c ){
+	Palabra p;
+	bool esRaro = true;
+	
+	for( int i = 0; i < p.SIZE-1; i++ ){
+		if( c == p.caracteres[i] )
+			esRaro = false;
+	}
+	
+	return esRaro;
 }
 
 char * Texto::convertirMinusculas( string aConvertir ){
